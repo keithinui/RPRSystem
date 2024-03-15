4069b844f8851d1059d3c2df241adcd6deb33a0b
keithinui <55673645+keithinui@users.noreply.github.com>
2024-03-15 19:02:13 +0900

    Update webRTC.js

--- RPRS_D2/webRTC.js
+++ RPRS_D2/webRTC.js
@@ -79,7 +79,7 @@ var volumeLevel;			// Volume level of phone side (patient side)
 
     room.once('open', () => {
       messages.textContent = '=== You joined ===\n';
-      joinTrigger.style.display = 'none';
+//      joinTrigger.style.display = 'none';
       leaveTrigger.style.display = 'block';
       // Start the timer to get the Statistics data by getStats()
       //timer2 = setInterval("onStatisticsTimer()", 2000);
@@ -175,7 +175,7 @@ var volumeLevel;			// Volume level of phone side (patient side)
       volumeUp.removeEventListener('click', onClickVolumeUp);
       volumeDown.removeEventListener('click', onClickVolumeDown);
       messages.textContent += '== You left ===\n';
-      leaveTrigger.style.display = 'none';
+//      leaveTrigger.style.display = 'none';
       joinTrigger.style.display = 'block';
 
       // Before cloasing send command to stop sendeing data
