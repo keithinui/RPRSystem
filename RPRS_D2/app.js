392d24bad54c114297a978bb3a97d06f103222f2
keithinui <55673645+keithinui@users.noreply.github.com>
2024-03-15 17:34:56 +0900

    Update app.js

--- RPRS_D2/app.js
+++ RPRS_D2/app.js
@@ -23,14 +23,7 @@ var startTime = 0;                // Rehabilitation time
 // Timer to get the Statistics data by getStats()
 // ********************************************************************
 function onStatisticsTimer() {
-//    const stats = await publication.getStats(subscriber);
-    // stats is [{},{},{},...]
-//    stats.forEach((report) => {
-        // When report is `RTCCodecStats` Object.
-//        if(report.type == "codec") {
             console.log("Timer for getStats!!"); // 90000
-//        }
-//    });
 }
 
 // ********************************************************************
