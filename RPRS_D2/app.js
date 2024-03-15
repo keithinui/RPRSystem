936f99e8f4cae27c095909077d250d327a380143
keithinui <55673645+keithinui@users.noreply.github.com>
2024-03-15 19:26:58 +0900

    Update app.js

--- RPRS_D2/app.js
+++ RPRS_D2/app.js
@@ -15,6 +15,7 @@ var timer1 =0;                    // Interval timer for rehabilitation time
 var timer2 =0;                    // Interval timer for getStats() API
 var sendWaveforms = 0;            // 0: Send waveforms Off,      1: On
 var calCommand = 0;               // 0: Cal Off,                 1: On
+var ltDisplayOriginal ;
 var startTime = 0;                // Rehabilitation time
 
 
@@ -65,6 +66,7 @@ window.onload = function () {
 
   //-------------------------------------------------------------------
   // Hide Leave button
+  ltDisplayOriginal  = leaveTrigger.style.display;
   leaveTrigger.style = "background:#00F00F";
   leaveTrigger.style.display = 'none';
 
